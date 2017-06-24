6e757d929bb283af6d96a18e9cdc8f5e59764967
ngthanh <thanh.nn1106@gmail.com>
2017-06-24 12:09:45 +0700

    Done show categories in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,9 +26,29 @@ class HomeController extends BaseController
     public function index()
     {
         $langCode = $this->lang;
+        $categories = \App\Categories::getRowByLang($langCode);
+        $filters = [];
+
+        foreach ($categories as $item) {
+            if (empty($item->parent_id)) {
+                $filters[$item->id] = array(
+                    'title' => $item->title,
+                    'slug' => $item->slug,
+                );
+            }
+            if(isset($filters[$item->parent_id])) {
+                $filters[$item->parent_id]['childs'][$item->id] = array(
+                    'id'    => $item->id,
+                    'title' => $item->title,
+                    'slug' => $item->slug,
+                );
+            }
+        }
+
         return view('home', [
             'langs' => \App\Languages::getResults(),
-            'generals' => \App\Generals::getResultsByLang($langCode)
+            'generals' => \App\Generals::getResultsByLang($langCode),
+            'categories' => $filters,
         ]);
     }
 }
