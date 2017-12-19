07e01a425255a6f1d986b89abdab05e14c165dc9
ngthanh <thanh.nn1106@gmail.com>
2017-12-20 02:49:39 +0700

    Add meta image

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -23,6 +23,7 @@
         <link rel="stylesheet" type="text/css" href="{{ asset_front('css/style.css') }}" >
         <link rel="stylesheet" type="text/css" href="{{ asset_front('css/style_custom.css') }}" >
         <link rel="shortcut icon" type="image/png" href="{{ asset_front('dataimages/favicon.png') }}"/>
+        <meta property="og:image" content="{{ asset_front('images/logo.png') }}">
 <!--         Start of allelua Zendesk Widget script 
         <script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(e){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var e=this.createElement("script");n&&(this.domain=n),e.id="js-iframe-async",e.src="https://assets.zendesk.com/embeddable_framework/main.js",this.t=+new Date,this.zendeskHost="allelua.zendesk.com",this.zEQueue=a,this.body.appendChild(e)},o.write('<body onload="document._l();">'),o.close()}();
 /*]]>*/</script>
